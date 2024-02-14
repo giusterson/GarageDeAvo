6aa768dc6f4daad216fc3cea9716c82dd098ae5d
CH\ORabarijaona <orb@aim-services.ch>
2024-02-14 16:40:22 +0100

    SQL dump init OK

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,5 +1,1 @@
-composer install -n
-php bin/console d:m:m --no-interaction
-php bin/console doc:fix:load --no-interaction
-php bin/console app:create-user demo@demo.com password
 exec apache2-foreground
